913514febc6397e31870e65ca9a66340c50d90e2
hirorich <ch.dim7ir@gmail.com>
2020-04-25 16:17:28 +0900

    テストデータの配置変更

--- src/main.py
+++ src/main.py
@@ -9,11 +9,11 @@ from service import multimedia_converter
 if __name__ == "__main__":
     
     # 動画解析処理実行
-    # result = multimedia_analyzer.exec('{"service_request":{"input_file_name":"../input2.mp4"}}')
+    # result = multimedia_analyzer.exec('{"service_request":{"input_file_name":"../../input2.mp4"}}')
     # print(result)
     
     # 動画変換処理実行
-    result = multimedia_converter.exec('{"service_request":{"input_file_bean":[{"input_file_name":"../input2.mp4","start_time":2.1666,"trim_duration":5.25,"start_frame":11,"frame_number":100,"frame_specification_flag":false,"video_fade_in_duration":1,"video_fade_out_duration":2,"audio_fade_in_duration":1,"audio_fade_out_duration":2}],"output_file_bean":{"convert_mode":1,"overwriting_flag":true,"output_file_name":"./_output/video/output.mp4","codec_type_combination":1}}}')
+    result = multimedia_converter.exec('{"service_request":{"input_file_bean":[{"input_file_name":"../../input2.mp4","start_time":2.1666,"trim_duration":5.25,"start_frame":11,"frame_number":100,"frame_specification_flag":false,"video_fade_in_duration":1,"video_fade_out_duration":2,"audio_fade_in_duration":1,"audio_fade_out_duration":2}],"output_file_bean":{"convert_mode":1,"overwriting_flag":true,"output_file_name":"./_output/video/output.mp4","codec_type_combination":1}}}')
     print(result)
     
 
