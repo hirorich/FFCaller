9432b76297232f611d783c848b0eb048ae1c794a
hirorich <ch.dim7ir@gmail.com>
2020-05-15 23:47:34 +0900

    512x512 でアイコン出力

--- src/logo.py
+++ src/logo.py
@@ -7,10 +7,7 @@ import cv2
 # 座標リスト取得
 def create_pos_dict(kame, l):
     
-    kame.getscreen().setworldcoordinates(0, 0, 511, 511)
-    
     kame.penup()
-    kame.shapesize(1)
     
     kame.setpos(255, 0)
     p0 = (255, 0)
@@ -90,8 +87,6 @@ def create_pos_dict(kame, l):
 # 描写
 def write_turtle(kame, pos_dict, color1='blue', color2='yellow'):
     
-    kame.shapesize(1)
-    
     point = pos_dict['p' + str(len(pos_dict) - 1)]
     kame.setpos(point[0], point[1])
     kame.color(color1, color1)
@@ -116,37 +111,88 @@ def write_turtle(kame, pos_dict, color1='blue', color2='yellow'):
 # 描写
 def create_image(pos_dict, color1='blue', color2='yellow'):
     
-    # Turtle は左下が原点
-    # opencv は左上が原点
-    
-    output_filename = 'icon.png'
+    # 出力画像画素データ
     img_data = np.zeros((512, 512, 4), np.uint8)
     
-    # blue
-    img_data[100:200, :, 0] = 255
-    img_data[100:200, :, 3] = 255
+    # ==================================================
+    # 左半分を作成
+    # y = ((p1y - p0y)x + p1x*p0y - p0x*p1y) / (p1x - p0x)
+    # y = (dY*x + XY)/dX
+    # ==================================================
+    # dY = p1y - p0y
+    dY_h = pos_dict['p3'][1] - pos_dict['p2'][1]
+    dY_l = pos_dict['p0'][1] - pos_dict['p1'][1]
     
-    # green
-    img_data[300:400, :, 1] = 255
-    img_data[300:400, :, 3] = 255
+    # XY = p1x*p0y - p0x*p1y
+    XY_h = pos_dict['p3'][0] * pos_dict['p2'][1] - pos_dict['p2'][0] * pos_dict['p3'][1]
+    XY_l = pos_dict['p0'][0] * pos_dict['p1'][1] - pos_dict['p1'][0] * pos_dict['p0'][1]
     
-    # red
-    img_data[:, 200:300, 2] = 255
-    img_data[:, 200:300, 3] = 255
+    # dX =p1x - p0x
+    dX_h = pos_dict['p3'][0] - pos_dict['p2'][0]
+    dX_l = pos_dict['p0'][0] - pos_dict['p1'][0]
+    for x in range(int(np.round(pos_dict['p2'][0])), int(np.round(pos_dict['p3'][0])+1)):
+        y_h = int(np.round((dY_h * x + XY_h) / dX_h))
+        y_l = int(np.round((dY_l * x + XY_l) / dX_l))
+        img_data[y_l:y_h, x, (0, 3)] = 255
+    # ==================================================
+    # dY = p1y - p0y
+    dY_h = pos_dict['p4'][1] - pos_dict['p5'][1]
+    dY_l = pos_dict['p10'][1] - pos_dict['p9'][1]
     
-    cv2.imwrite(output_filename, img_data)
+    # XY = p1x*p0y - p0x*p1y
+    XY_h = pos_dict['p4'][0] * pos_dict['p5'][1] - pos_dict['p5'][0] * pos_dict['p4'][1]
+    XY_l = pos_dict['p10'][0] * pos_dict['p9'][1] - pos_dict['p9'][0] * pos_dict['p10'][1]
+    
+    # dX =p1x - p0x
+    dX_h = pos_dict['p4'][0] - pos_dict['p5'][0]
+    dX_l = pos_dict['p10'][0] - pos_dict['p9'][0]
+    for x in range(int(np.round(pos_dict['p5'][0])+1), int(np.round(pos_dict['p4'][0])+1)):
+        y_h = int(np.round((dY_h * x + XY_h) / dX_h)) - 1
+        y_l = int(np.round((dY_l * x + XY_l) / dX_l)) + 1
+        img_data[y_l:y_h, x, (0, 3)] = 0
+    # ==================================================
+    # dY = p1y - p0y
+    dY_h = pos_dict['p7'][1] - pos_dict['p6'][1]
+    dY_l = pos_dict['p8'][1] - pos_dict['p9'][1]
+    
+    # XY = p1x*p0y - p0x*p1y
+    XY_h = pos_dict['p7'][0] * pos_dict['p6'][1] - pos_dict['p6'][0] * pos_dict['p7'][1]
+    XY_l = pos_dict['p8'][0] * pos_dict['p9'][1] - pos_dict['p9'][0] * pos_dict['p8'][1]
+    
+    # dX =p1x - p0x
+    dX_h = pos_dict['p7'][0] - pos_dict['p6'][0]
+    dX_l = pos_dict['p8'][0] - pos_dict['p9'][0]
+    for x in range(int(np.round(pos_dict['p6'][0])), int(np.round(pos_dict['p7'][0])+1)):
+        y_h = int(np.round((dY_h * x + XY_h) / dX_h))
+        y_l = int(np.round((dY_l * x + XY_l) / dX_l))
+        img_data[y_l:y_h, x, (0, 3)] = 255
+    # ==================================================
+    # Turtle は左下が原点、opencv は左上が原点なので上下反転
+    img_data = np.flipud(img_data)
+    
+    # 上下左右反転したものを右半分に張り付け
+    img_data += np.flip(img_data, (0, 1))[:, :, (1, 0, 0, 3)]
+    
+    # 画像出力
+    cv2.imwrite('icon_512x512.png', img_data)
 
 # main
 if __name__ == "__main__":
     
     l = 64
     
-    # 座標取得
+    # Turtle設定
     kame = Turtle()
+    kame.getscreen().setworldcoordinates(0, 0, 511, 511)
+    kame.shapesize(1)
+    kame.shape('turtle')
+    kame.speed(10)
+    
+    # 座標取得
     pos_dict = create_pos_dict(kame, l)
     
     # 座標確認
-    #write_turtle(kame, pos_dict)
+    write_turtle(kame, pos_dict)
     
     # 画像出力
     create_image(pos_dict)
