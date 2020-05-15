ffac4e4f71ac25babb8c9f538793a8f0aab41d39
hirorich <ch.dim7ir@gmail.com>
2020-05-16 00:19:12 +0900

    アイコン作成

--- create_icon/icon.bat
+++ create_icon/icon.bat
@@ -7,7 +7,14 @@ rem envファイル読み込み
 call ..\src\env\loadenv.bat env\icon.env
 call conda activate %env%
 
+rem フォルダ作成
+if not exist icon mkdir icon
+del /Q icon\*
+
 rem 実行
 python icon.py
+
+rem アイコン作成
+convert ./icon/icon_512x512.png -define icon:auto-resize ./icon/icon.ico
 
 pause
