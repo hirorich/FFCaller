77b94e706e929524235de319c33175bf70643952
hirorich <ch.dim7ir@gmail.com>
2020-09-13 02:40:54 +0900

    sqlite3共通部品拡張

--- common/utility/sqlite3_utils.py
+++ common/utility/sqlite3_utils.py
@@ -21,3 +21,25 @@ def fetchone(db_filename, query, param):
     # 取得結果を返却
     # 対象行が0行だった場合はNoneが返却される
     return result
+
+# 複数行取得
+def fetchall(conn, query, param=None):
+    cursor = conn.cursor()
+    
+    # クエリ実行
+    if param is None:
+        cursor.execute(query)
+    else:
+        cursor.execute(query, param)
+    
+    return cursor.fetchall()
+
+# クエリ実行
+def execute(conn, query, param=None):
+    cursor = conn.cursor()
+    
+    # クエリ実行
+    if param is None:
+        cursor.execute(query)
+    else:
+        cursor.execute(query, param)
