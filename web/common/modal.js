3cdfb46e1a3d7465a370ba876915bb1b35e0972d
hirorich <ch.dim7ir@gmail.com>
2020-09-15 00:50:33 +0900

    モーダル表示は共通フォルダに配置

---
+++ web/common/modal.js
@@ -0,0 +1,30 @@
+// モーダル画面
+// slotでtitle, body, buttonを指定
+const modal_component = {
+    template: `
+        <div class="modal fade">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <slot name="title"></slot>
+                    </div>
+                    <div class="modal-body">
+                        <slot name="body"></slot>
+                    </div>
+                    <div class="modal-footer">
+                        <slot name="button"></slot>
+                        <button class="btn btn-secondary" v-on:click="close_modal()">閉じる</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `,
+    methods: {
+        show_modal: function() {
+            $(this.$el).modal('show');
+        },
+        close_modal: function() {
+            $(this.$el).modal('hide');
+        }
+    }
+}
