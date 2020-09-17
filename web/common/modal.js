22bac4108b6891c9ee0d126c28f2904c224c30a2
hirorich <ch.dim7ir@gmail.com>
2020-09-18 07:15:32 +0900

    モーダル開閉の際に再描画するか否か指定可能

--- web/common/modal.js
+++ web/common/modal.js
@@ -1,15 +1,27 @@
 // モーダル画面
 // slotでtitle, body, buttonを指定
+// 開閉のたびに再描画するかをcanRepaintで指定可能
 const modal_component = {
+    props: {
+        canRepaint: {
+            type: Boolean,
+            default: false
+        },
+    },
     data: function() {
         return {
             is_show: false
         }
     },
+    computed: {
+        can_repaint : function() {
+            return this.canRepaint;
+        }
+    },
     template: `
         <div class="modal fade" data-backdrop="static">
             <div class="modal-dialog">
-                <div v-if="is_show" class="modal-content">
+                <div v-if="can_repaint || is_show" class="modal-content">
                     <div class="modal-header">
                         <slot name="title"></slot>
                     </div>
