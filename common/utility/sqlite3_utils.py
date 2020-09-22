79c2c8ed08e11a1e68190d557e9a81746c8484ae
hirorich <ch.dim7ir@gmail.com>
2020-09-22 15:12:34 +0900

    クエリ実行部分を共通化

--- common/utility/sqlite3_utils.py
+++ common/utility/sqlite3_utils.py
@@ -6,13 +6,7 @@ import sqlite3
 
 # 1行取得
 def fetchone(conn, query, param=None):
-    cursor = conn.cursor()
-    
-    # クエリ実行
-    if param is None:
-        cursor.execute(query)
-    else:
-        cursor.execute(query, param)
+    cursor = __execute(conn, query, param)
     
     # 取得結果を返却
     # 対象行が0行だった場合はNoneが返却される
@@ -20,18 +14,16 @@ def fetchone(conn, query, param=None):
 
 # 複数行取得
 def fetchall(conn, query, param=None):
-    cursor = conn.cursor()
-    
-    # クエリ実行
-    if param is None:
-        cursor.execute(query)
-    else:
-        cursor.execute(query, param)
+    cursor = __execute(conn, query, param)
     
     return cursor.fetchall()
 
 # クエリ実行
 def execute(conn, query, param=None):
+    __execute(conn, query, param)
+
+# クエリ実行
+def __execute(conn, query, param=None):
     cursor = conn.cursor()
     
     # クエリ実行
@@ -39,3 +31,5 @@ def execute(conn, query, param=None):
         cursor.execute(query)
     else:
         cursor.execute(query, param)
+    
+    return cursor
