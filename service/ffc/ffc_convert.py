67aada55a36252192463b8f29a73e2a46112bc4e
hirorich <ch.dim7ir@gmail.com>
2020-09-27 15:38:16 +0900

    時間<->フレーム変換の修正

--- service/ffc/ffc_convert.py
+++ service/ffc/ffc_convert.py
@@ -11,7 +11,7 @@ def frame_to_time(start_frame, end_frame, r_frame_rate):
     r_frame_rate_denom = int(r_frame_rate.split('/')[1])
     
     start_time = _to_float(Decimal(start_frame - 1) * r_frame_rate_denom / r_frame_rate_numer)
-    end_time = _to_float(Decimal(end_frame - 1) * r_frame_rate_denom / r_frame_rate_numer)
+    end_time = _to_float(Decimal(end_frame) * r_frame_rate_denom / r_frame_rate_numer)
     
     return start_time, end_time
 
@@ -21,7 +21,7 @@ def time_to_frame(start_time, end_time, r_frame_rate):
     r_frame_rate_denom = int(r_frame_rate.split('/')[1])
     
     start_frame = _to_int(Decimal(start_time) * r_frame_rate_numer / r_frame_rate_denom) + 1
-    end_frame = _to_int(Decimal(end_time) * r_frame_rate_numer / r_frame_rate_denom) + 1
+    end_frame = _to_int(Decimal(end_time) * r_frame_rate_numer / r_frame_rate_denom)
     
     return start_frame, end_frame
 
