8e81503aab5ab22651e8f24e7f3fcb96ce3d60d7
hirorich <ch.dim7ir@gmail.com>
2020-10-03 22:44:05 +0900

    マージ時のバグ修正

--- service/marger/marge_ffmpeg.py
+++ service/marger/marge_ffmpeg.py
@@ -11,7 +11,7 @@ from service.marger import command_creater
 def marge(output_bean):
     
     # コマンド作成
-    command_creater.create(output_bean)
+    command = command_creater.create(output_bean)
     
     # 実行
     proc_stdout = command_utils.run(command, True)
