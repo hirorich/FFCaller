04d4a1b021344d0d8e67ffe21e73007fa041d9cb
hirorich <ch.dim7ir@gmail.com>
2020-10-17 14:35:24 +0900

    ロード中の画面ボタン制御

--- web/index.js
+++ web/index.js
@@ -4,15 +4,29 @@ const vm = new Vue({
     components: {
         'target-component': target_component
     },
+    data: function() {
+        return {
+            is_loading: false
+        }
+    },
     template: `
         <div>
-            <button class="btn btn-primary" v-on:click="add_target()">追加</button>
+            <button class="btn btn-primary" v-bind:disabled="is_loading" v-on:click="add_target()">
+                <span v-if="is_loading" class="spinner-border spinner-border-sm"></span>
+                <span v-if="is_loading">Loading...</span>
+                <span v-else="is_loading">追加</span>
+            </button>
             <target-component ref="target"></target-component>
         </div>
     `,
     methods: {
         add_target: function() {
             eel.ffc_request_choose_files();
+            this.is_loading = true;
+        },
+        set_target: function(response) {
+            this.$refs.target.set_target_list(response);
+            this.is_loading = false;
         }
     }
 });
@@ -20,5 +34,5 @@ const vm = new Vue({
 // ターゲット一覧取得
 eel.expose(ffc_response_target_list)
 function ffc_response_target_list(response) {
-    vm.$refs.target.set_target_list(response);
+    vm.set_target(response);
 }
