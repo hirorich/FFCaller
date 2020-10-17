7cac13eda116b205a5da89fbd4226be7cbc6b6e4
hirorich <ch.dim7ir@gmail.com>
2020-10-18 01:45:44 +0900

    動画情報にファイルパスを表示

--- service/ffc/ffc_response_media_info.py
+++ service/ffc/ffc_response_media_info.py
@@ -12,12 +12,15 @@ def exec(conn, target_id):
         # 動画情報取得
         target = ffc_select.get_target(conn, target_id)
         file_id = target.file_id
+        file = ffc_select.get_file(conn, file_id)
         format = ffc_select.get_format(conn, file_id)
         video_stream = ffc_select.get_video_stream(conn, file_id)
         audio_streams = ffc_select.get_audio_streams(conn, file_id)
         
         # 動画情報設定
         response = dict()
+        response['file'] = dict()
+        response['file']['filepath'] = file.filepath
         response['format'] = format.to_dict()
         if video_stream is not None:
             response['video_stream'] = video_stream.to_dict()
